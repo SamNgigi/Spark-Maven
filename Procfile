f600a2fc824ed954b9de01f8d7f65843002561ad
SamNgigi <sam.t.ngigi@gmail.com>
2018-11-14 22:31:26 +0300

    Application error

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/maven-spark-1.0-jar-with-dependencies.jar
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,6 +16,6 @@ public class App {
 
     public static void main (String[] args) {
         BasicConfigurator.configure();
-        get("/", (request, response) -> "Hello Maven!");
+        get("/", (request, response) -> "Hello Maven! Am the best baby!!");
     }
 }
