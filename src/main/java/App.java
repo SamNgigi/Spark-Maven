78edbc17cfc1d657cf7313f38caae8110b2bed15
SamNgigi <sam.t.ngigi@gmail.com>
2018-11-14 22:02:53 +0300

    Added function to assign port to deployed app.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,15 @@ import static spark.Spark.*;
 
 public class App {
     final static Logger logger = Logger.getLogger(App.class);
+
+    static int getHerokuAssignedPort () {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
     public static void main (String[] args) {
         BasicConfigurator.configure();
         get("/", (request, response) -> "Hello Maven!");
