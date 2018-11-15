86f2d5c2e4de7b3756f02ab73b47d331fe9cdc7e
SamNgigi <sam.t.ngigi@gmail.com>
2018-11-15 07:00:33 +0300

    Deployed successfully. Now removing some git cached files to do final deploy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,6 @@ public class App {
     public static void main (String[] args) {
         BasicConfigurator.configure();
         port(getHerokuAssignedPort());
-        get("/", (request, response) -> "Hello Maven! Am the best baby!!");
+        get("/", (request, response) -> "Hello Maven!\nI am the best that ever was and will be!!\nI am plethorically blessed!\nI am so happy and so very grateful for the fact fact that I am now earning $1 million a second.");
     }
 }
