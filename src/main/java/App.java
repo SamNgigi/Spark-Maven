f9d672dbede0efaeec97d99a2567d1f2141768f9
SamNgigi <sam.t.ngigi@gmail.com>
2018-11-15 12:19:04 +0300

    Added comments to the application.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,8 +4,9 @@ import org.apache.log4j.BasicConfigurator;
 import static spark.Spark.*;
 
 public class App {
+    /* sl4j logger manenos */
     final static Logger logger = Logger.getLogger(App.class);
-
+    /* Responsible for getting the heroku port for our app.  */
     static int getHerokuAssignedPort () {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
@@ -15,7 +16,9 @@ public class App {
     }
 
     public static void main (String[] args) {
+        /* sl4j logger manenos */
         BasicConfigurator.configure();
+       /*  Assigning the heroku port to our app. */
         port(getHerokuAssignedPort());
         get("/", (request, response) -> "Hello Maven!\nI am the best that ever was and will be!!\nI am plethorically blessed!\nI am so happy and so very grateful for the fact fact that I am now earning $1 million a second.");
     }
